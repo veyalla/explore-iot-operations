a0e33ef9e4961f62b4e991c8ec0de81c675890e9
Venkat Yalla <veyalla@microsoft.com>
2024-11-26 18:30:28 -0800

    Update postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,3 +3,9 @@ echo 'export CLUSTER_NAME=${CODESPACE_NAME%-*}' >> ~/.bashrc
 source ~/.bashrc
 
 echo -e "Environment: \nSUBSCRIPTION_ID: $SUBSCRIPTION_ID \nRESOURCE_GROUP: $RESOURCE_GROUP \nLOCATION: $LOCATION \nCLUSTER_NAME: $CLUSTER_NAME"
+
+# Create k3d cluster and forwarded ports
+k3d cluster delete
+k3d cluster create \
+-p '1883:1883@loadbalancer' \
+-p '8883:8883@loadbalancer'
