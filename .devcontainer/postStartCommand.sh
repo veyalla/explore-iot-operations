5580315a6763cd15ed1f819335bbd49479356f57
Venkat Yalla <veyalla@microsoft.com>
2024-12-17 18:57:41 +0000

    fix

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,5 @@
 echo 'export CLUSTER_NAME=${CODESPACE_NAME%-*}' >> ~/.bashrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 source ~/.bashrc
 
 echo -e "Environment: \nSUBSCRIPTION_ID: $SUBSCRIPTION_ID \nRESOURCE_GROUP: $RESOURCE_GROUP \nLOCATION: $LOCATION \nCLUSTER_NAME: $CLUSTER_NAME"
