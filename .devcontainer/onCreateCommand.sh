0c916d2b4c02dde299581d6211e3cb4f12d214ad
Venkat Yalla <veyalla@microsoft.com>
2025-02-16 16:15:15 -0800

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -6,11 +6,4 @@ set -o pipefail
 
 echo "Starting On Create Command"
 
-# Create k3d cluster and forwarded ports
-k3d cluster delete
-k3d cluster create \
--i rancher/k3s:v1.32.1-rc2-k3s1 \
--p '1883:1883@loadbalancer' \
--p '8883:8883@loadbalancer'
-
 echo "Ending On Create Command"
